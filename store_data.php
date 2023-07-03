48fff17d7fd42a5258c55cc863243681af2aff31
Josephat-Onkoba <josephatonkoba30@gmail.com>
2023-07-03 22:20:58 +0300

    Build a simple html form that collects data of a user and stores it in a database

---
+++ store_data.php
@@ -0,0 +1,37 @@
+<?php
+// Connect to the database
+$servername = "localhost:3306";
+$username = "root";
+$password = "Password254";
+$dbname = "sms";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Retrieve data from table
+$sql = "SELECT * FROM Form_data";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table>";
+    echo "<tr><th>Name</th><th>Email</th><th>Phone</th><th>Address</th><th>City</th></tr>";
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>" . $row['name'] . "</td>";
+        echo "<td>" . $row['email'] . "</td>";
+        echo "<td>" . $row['phone'] . "</td>";
+        echo "<td>" . $row['address'] . "</td>";
+        echo "<td>" . $row['city'] . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+} else {
+    echo "No data found.";
+}
+
+$conn->close();
+?>
